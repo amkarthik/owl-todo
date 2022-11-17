e5a3cae4ee5dbe6bd4be4404101cdf299d90e34b
Karthik, Sodexis <karthik@sodexis.com>
2022-11-17 18:47:30 +0530

    12. Filtering tasks 13. The Final Touch

--- app/app.js
+++ app/app.js
@@ -48,8 +48,8 @@ function createTaskStore() {
 class Task extends Component {
   static template = xml /*xml*/ `
   <div class="task" t-att-class="props.task.isCompleted ? 'done' : ''">
-  <input type="checkbox" t-att-checked="props.task.isCompleted" t-on-click="() => store.toggleTask(props.task)" />
-  <span><t t-esc="props.task.text" /></span>
+  <input type="checkbox" t-att-id="props.task.id" t-att-checked="props.task.isCompleted" t-on-click="() => store.toggleTask(props.task)" />
+  <label t-att-for="props.task.id"><t t-esc="props.task.text" /></label>
   <span class="delete" t-on-click="() => store.deleteTask(props.task)">🗑</span>
   </div>
   `;
@@ -66,9 +66,19 @@ class Root extends Component {
   <div class="todo-app">
     <input placeholder="Enter a new task" t-on-keyup="addTask" t-ref="add-input"/>
     <div class="task-list">
-      <t t-foreach="store.tasks" t-as="task" t-key="task.id">
+      <t t-foreach="displayedTasks" t-as="task" t-key="task.id">
         <Task task="task"/>
       </t>
+    </div>
+    <div class="task-panel" t-if="store.tasks.length">
+      <div class="task-counter">
+        <t t-esc="displayedTasks.length" />
+        <t t-if="displayedTasks.length lt store.tasks.length"> / <t t-esc="store.tasks.length" />
+        </t> tasks (s)
+      </div>
+      <div>
+        <span t-foreach="['all', 'active', 'completed']" t-as="f" t-key="f" t-att-class="{active: filter.value===f}" t-on-click="() => this.setFilter(f)" t-esc="f" />
+      </div>
     </div>
   </div>
   `;
@@ -86,6 +96,20 @@ class Root extends Component {
     const inputRef = useRef("add-input");
     onMounted(() => inputRef.el.focus());
     this.store = useStore();
+    this.filter = useState({value: "all"});
+  }
+
+  get displayedTasks() {
+    const tasks = this.store.tasks;
+    switch (this.filter.value) {
+      case "active": return tasks.filter(t => !t.isCompleted);
+      case "completed": return tasks.filter(t => t.isCompleted);
+      case "all": return tasks;      
+    }
+  }
+
+  setFilter(filter) {
+    this.filter.value = filter;
   }
 }
 
